3130c1e4cded7c771740ab176f74cbf0aabb4cfb
Paul Hitz <ahappycamper+git@gmail.com>
2022-08-05 19:53:01 +0100

    Minor refactor

--- scripts/editor.js
+++ scripts/editor.js
@@ -62,5 +62,4 @@ class Editor {
     };
     img.src = dataUrl;
   }
-
 }
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,11 +5,19 @@ let canvas;
 let ctx;
 let images = [];
 let monster = {};
+let settings;
 
 /**
  * Initialise the canvas, pre-load the images and display a monster.
  */
 function init() {
+  //Load application settings (if any).
+  settings = JSON.parse(localStorage.getItem("monsters.settings"));
+  if (settings == null || settings.deletedExamples == null) {
+    settings = {deletedExamples : []};
+    localStorage.setItem("monsters.settings", JSON.stringify(settings));
+  }
+
   //When all images have loaded, render a random monster.
   if (images.length == 0) {
     loadImages(function() {
@@ -19,25 +27,9 @@ function init() {
     drawRandomMonster();
   }
 
-  //Add an event listener to the canvas. Allows the user to load individual monster parts.
-  if (canvas == null) {
-    canvas = document.getElementById('canvas');
-    canvas.addEventListener('mousedown', e => {
-      const yAxis = e.offsetY;
-      switch (true) {
-        case yAxis < SECTION_HEIGHT:
-          monster.head = randomImage(images);
-          break;
-        case yAxis > SECTION_HEIGHT * 2:
-          monster.legs = randomImage(images);
-          break;
-        default:
-          monster.torso = randomImage(images);
-      }
-      drawMonster(monster);
-    });
-    ctx = canvas.getContext('2d');
-  }
+  //Add an event listener to the canvas.
+  addMonsterListeners(canvas);
+  ctx = canvas.getContext('2d');
 
   //Generate a gallery of all the monster images. The gallery is hidden until selected.
   generateGallery();
@@ -49,22 +41,26 @@ function init() {
 }
 
 /**
- * Pre-load images so they can be rendered on the canvas.
+ * Pre-load images so they can be rendered on the canvas. A certain number of example
+ * images are used and localStorage is used for user generated images.
  */
 function loadImages(callback) {
   //First add the example images. (stored on the server)
   let loadedImages = 0;
   for (let i = 0; i < NUMBER_OF_EXAMPLES; i++) {
-    const img = createImage(i, `images/monsters/${i}.png`);
-    img.onload = function() {
-      if(++loadedImages >= NUMBER_OF_EXAMPLES) {
+    //Ignore example images that the user has marked as deleted.
+    if (settings.deletedExamples.indexOf("" + i) === -1) {
+      const img = createImage(i, `images/monsters/${i}.png`);
+      img.onload = function() {
+        if(++loadedImages >= (NUMBER_OF_EXAMPLES - settings.deletedExamples.length)) {
           callback();
-      }
-    };
-    images[i] = img;
+        }
+      };
+      images.push(img);
+    }
   }
 
-  //Add the images in localStorage (if any).
+  //Add the images from localStorage (if any).
   for (let i = 0; i < localStorage.length; i++) {
     const key = localStorage.key(i);
     if (isValidKey(key)) {
@@ -97,8 +93,9 @@ function drawRandomMonster() {
 
 /**
  * Draw the supplied monster on the canvas. A monster consists of a random head, torso and legs.
- * 
- * TODO consider adding the body part height and the 'y' co-ord to the monster object rather than specifying it here.
+ *
+ * TODO consider adding the body part height and the 'y' co-ord to the monster object rather
+ * than specifying it here.
  */
 function drawMonster(myMonster) {
   monster = myMonster;
@@ -125,7 +122,7 @@ function download() {
         downloadLink.click();
         notie.alert({ type: "success", position: "bottom", text: "Downloading monster..." });
       } catch(e) {
-        console.log(e);
+        console.error(e);
         notie.alert({ type: "error", position: "bottom", text: "Failed to download monster :(" });
       }
     }
@@ -144,10 +141,6 @@ function toggleView(view) {
       document.getElementById("create").style.display = "none";
       document.getElementById("reload-button").style.display = "none";
       document.getElementById("option-buttons").style.display = "none";
-      //TODO Rather than all the generateGallery calls everywhere, should we just do it here?
-      // if (galleryUpdated) {
-      //   //refresh the page
-      // }
       break;
     case view === "editor":
       document.getElementById("gallery").style.display = "none";
@@ -188,16 +181,12 @@ function generateGallery() {
 
 /**
  * Display the gallery preview area and configure it for the selected image.
- * 
- * TODO scroll to the top of the page?
  */
 function displayGalleryPreview(img) {
-  const index = images.indexOf(img);
-
   document.getElementById("gallery-preview").style.display = "block";
   document.getElementById("preview-image").src = img.src;
   document.getElementById("preview-view-button").onclick = function() {
-    drawSpecificMonster(index);
+    drawSpecificMonster(images.indexOf(img));
     toggleView('monster');
   };
   document.getElementById("preview-edit-button").onclick = function() {
@@ -206,6 +195,31 @@ function displayGalleryPreview(img) {
   document.getElementById("preview-delete-button").onclick = function() {
     deleteMonster(img.id);
   };
+  scroll(0, 0);
+}
+
+/**
+ * Add a listener to the main monster canvas. The listener allows the
+ * user to load individual monster parts.
+ */
+function addMonsterListeners(canvas) {
+  if (canvas == null) {
+    canvas = document.getElementById('canvas');
+    canvas.addEventListener('mousedown', e => {
+      const yAxis = e.offsetY;
+      switch (true) {
+        case yAxis < SECTION_HEIGHT:
+          monster.head = randomImage(images);
+          break;
+        case yAxis > SECTION_HEIGHT * 2:
+          monster.legs = randomImage(images);
+          break;
+        default:
+          monster.torso = randomImage(images);
+      }
+      drawMonster(monster);
+    });
+  }
 }
 
 /**
@@ -224,7 +238,8 @@ function addEditorListeners(editorCanvas) {
   });
   ['mousemove', 'touchmove'].forEach(function(e) {
     editorCanvas.addEventListener(e, function(event) {
-      event.preventDefault(); //Prevent normal browser processing of the event. e.g. scrolling instead of drawing.
+      //Prevent normal browser processing of the event. e.g. scrolling instead of drawing.
+      event.preventDefault();
       monsterEditor.draw(event);
     });
   });
@@ -232,8 +247,6 @@ function addEditorListeners(editorCanvas) {
 
 /**
  * Save the current monster to localStorage.
- * 
- * TODO Consider disabling the save button to avoid multiple clicks.
  */
 function saveMonster() {
   try {
@@ -243,26 +256,29 @@ function saveMonster() {
     localStorage.setItem(key, src);
     notie.alert({ type: "success", position: "bottom", text: "Your monster has been successfully saved. You can view it in the gallery." });
 
-    //Add it to the images array and the gallery.
+    //Add it to the images array and update the gallery.
     images.push(createImage(key, src));
     generateGallery();
 
   } catch(e) {
-    console.log(e);
+    console.error(e);
     notie.alert({ type: "error", position: "bottom", text: "An error occurred while saving the monster." });
   }
 }
 
 /**
- * Delete the specified monster from localStorage and from the UI.
- * 
- * TODO permanently handle deleted example images. Use localStorage? monsters.settings
+ * Delete the specified monster from localStorage and from the UI. It's not possible to
+ * delete the example images so they need to be handled differently.
  */
 function deleteMonster(id) {
   notie.confirm({
     text: 'Are you sure?',
     submitCallback: function() {
-      localStorage.removeItem(id);
+      if (isValidKey(id)) {
+        localStorage.removeItem(id);
+      } else {
+        AddToIgnoredExamples(id);
+      }
       images.splice(findArrayIndex(id, images), 1);
       generateGallery();
       notie.alert({ type: "success", position: "bottom", text: "Success! The monster decided to go home..." });
@@ -304,7 +320,7 @@ function editMonster(id) {
 
 
 /*
- * Various helper functions 
+ * Various helper functions
  */
 
 
@@ -325,7 +341,9 @@ function editMonster(id) {
   }
 }
 
-//Given an array and an ID, find the array index that matches that ID.
+/**
+ * Given an array and an ID, find the array index that matches that ID.
+ */
 function findArrayIndex(id, arr) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i].id === id) {
@@ -334,14 +352,14 @@ function findArrayIndex(id, arr) {
   }
 }
 
-//
+/**
+ * Create and return a new Image element with the specified id and src.
+ */
 function createImage(key, src) {
-  //If src is null then use the key to retrieve from localStorage.
-  //if (isValidKey(key)) {
+  //If src is null then use the key to retrieve it from localStorage.
   if (src == null) {
     src = localStorage.getItem(key);
   }
-    
   const img = new Image();
   img.id = key;
   img.src = src;
@@ -351,20 +369,11 @@ function createImage(key, src) {
   return img;
 }
 
+/**
+ * Since we can't actually delete the example images, we track what ones have been deleted.
+ */
+ function AddToIgnoredExamples(id) {
+  settings.deletedExamples.push(id);
+  localStorage.setItem("monsters.settings", JSON.stringify(settings));
+}
 
-
-
-
-
-
-
-
-/*
-TODO
--Integrate with existing monster (main and gallery) functionality. Move away from the default images. Add settings to localstorage. Copy default images to local storage.
-
--Sort out what to do with the default images. consider options. toggle for them? add to localstorage? delete them? handle them seamlessly behind the scenes (some images from localstorage, some from directory with fake delete)? Handle them seamlessly is the way to go.
-
--Only refresh gallery when gallery view selected and localStorage modified?
-
-*/
