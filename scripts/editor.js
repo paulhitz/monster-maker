61dc32e9ea5bd15f8e17dea83a920a03a986d2de
Paul Hitz <ahappycamper+git@gmail.com>
2022-08-29 15:55:37 +0100

    Allow the editor to resize on small screens

--- scripts/editor.js
+++ scripts/editor.js
@@ -6,7 +6,9 @@ class Editor {
   ctx;
   drawing = false;
 
-  //Initialise and set defaults.
+  /**
+   * Initialise and set defaults.
+   */
   constructor(canvas) {
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d');
@@ -31,29 +33,43 @@ class Editor {
     }
   }
 
-  //Add the current mouse or touch co-ordinates to the current line (lineTo) and render it (stroke) immediately.
+  /**
+   * Add the current mouse or touch co-ordinates to the current line (lineTo) and render it (stroke) immediately.
+   *
+   * Note on offsets: The canvas will be resized on devices with small viewports (e.g. from 600px to 400px).
+   * Consequently we need an offset percentage to account for that. We have an additional offset to account
+   * for padding, margins etc.
+   */
   draw(e) {
     if (this.drawing) {
       let event = e;
       if (event.type === "touchmove") {
         event = event.touches[0];
       }
-      this.ctx.lineTo(event.pageX - this.canvas.offsetLeft, event.pageY - this.canvas.offsetTop);
+
+      let offsetWidthPercentage = this.canvas.width / this.canvas.offsetWidth;
+      this.ctx.lineTo((event.pageX - this.canvas.offsetLeft) * offsetWidthPercentage, event.pageY - this.canvas.offsetTop);
       this.ctx.stroke();
     }
   }
 
-  //Clear the entire canvas. Retains transparency.
+  /**
+   * Clear the entire canvas. Retains transparency.
+   */
   clear() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 
-  //Return a base64 encoded PNG of the canvas content.
+  /**
+   * Return a base64 encoded PNG of the canvas content.
+   */
   export() {
     return this.canvas.toDataURL();
   }
 
-  //Add the provided image to the canvas.
+  /**
+   * Add the provided image to the canvas.
+   */
   import(dataUrl) {
     this.clear();
     const img = new Image();
